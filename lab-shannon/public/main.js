1749d7cf78e00199b523a28c4842535916d74269
Shannon Dillon <shannondillon@Shannons-MacBook-Pro.local>
2018-01-29 16:52:39 -0800

    add message to user telling them what their username is; clear form fields after submission

--- lab-shannon/public/main.js
+++ lab-shannon/public/main.js
@@ -11,13 +11,25 @@ let messageBoard = document.getElementById('messageBoard');
 usernameForm.addEventListener('submit', (event) => {
   event.preventDefault();
   let username = usernameInput.value;
+  let usernameSpace;
   socket.emit('set-username', {username: username});
+  usernameInput.value = '';
+
+  if(!document.getElementById('usernameSpace')){
+    usernameSpace = document.createElement('section');
+    usernameSpace.setAttribute('id', 'usernameSpace');
+    usernameSpace.textContent = `Your username is: ${username}`;
+    messageBoard.prepend(usernameSpace);
+  }
+  usernameSpace = document.getElementById('usernameSpace');
+  usernameSpace.textContent = `Your username is: ${username}`;
 });
 
 messageForm.addEventListener('submit', (event) => {
   event.preventDefault();
   let message = messageInput.value;
   socket.emit('send-message', {message: message});
+  messageInput.value = '';
 });
 
 socket.on('incoming-message', (data) => {
